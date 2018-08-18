c6f99b1f7518c553435dcee1fc68e45c41157fc7
FlowerWrong <sysuyangkang@gmail.com>
2018-08-18 20:33:36 +0800

    support socks 5 udp

--- k1/udp_relay.go
+++ k1/udp_relay.go
@@ -2,47 +2,79 @@ package k1
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"net"
 	"sync"
 	"time"
 
+	"github.com/FlowerWrong/gosocks"
 	"github.com/FlowerWrong/kone/tcpip"
+	socks5Proxy "github.com/FlowerWrong/proxy"
 )
 
 type UDPTunnel struct {
-	session *NatSession
-	record  *DomainRecord
-
-	cliaddr *net.UDPAddr
-
-	localConn  *net.UDPConn
-	remoteConn *net.UDPConn
+	session        *NatSession
+	clientAddr     *net.UDPAddr
+	localConn      *net.UDPConn
+	remoteUDPConn  *net.UDPConn
+	remoteTCPConn  net.Conn
+	remoteHostType byte
+	remoteHost     string
+	remotePort     uint16
+	BndHost        string
+	BndPort        uint16
 }
 
 func (tunnel *UDPTunnel) SetDeadline(duration time.Duration) error {
-	return tunnel.remoteConn.SetDeadline(time.Now().Add(duration))
+	err := tunnel.remoteTCPConn.SetDeadline(time.Now().Add(duration))
+	if err != nil {
+		return err
+	}
+	return tunnel.remoteUDPConn.SetDeadline(time.Now().Add(duration))
 }
 
 func (tunnel *UDPTunnel) Pump() error {
 	b := make([]byte, MTU)
 	for {
-		n, err := tunnel.remoteConn.Read(b)
+		n, err := tunnel.remoteUDPConn.Read(b)
 		if err != nil {
 			if opErr, ok := err.(*net.OpError); ok && opErr.Timeout() {
 				return nil
 			}
 			return err
 		}
-		_, err = tunnel.localConn.WriteToUDP(b[:n], tunnel.cliaddr)
+		udpReq, err := gosocks.ParseUDPRequest(b[:n])
 		if err != nil {
 			return err
+		}
+
+		_, err = tunnel.localConn.WriteToUDP(udpReq.Data, tunnel.clientAddr)
+		if err != nil {
+			return err
+		}
+
+		// close if it is a dns query
+		if tunnel.remotePort == 53 {
+			return nil
 		}
 	}
 }
 
 func (tunnel *UDPTunnel) Write(b []byte) (int, error) {
-	return tunnel.remoteConn.Write(b)
+	req := &gosocks.UDPRequest{
+		Frag:     0,
+		HostType: tunnel.remoteHostType,
+		DstHost:  tunnel.remoteHost,
+		DstPort:  tunnel.remotePort,
+		Data:     b,
+	}
+
+	n, err := tunnel.remoteUDPConn.WriteTo(gosocks.PackUDPRequest(req), gosocks.SocksAddrToNetAddr("udp", tunnel.BndHost, tunnel.BndPort).(*net.UDPAddr))
+	if err != nil {
+		logger.Errorf("[udp] write to socks5 failed: %s", err)
+	}
+	return n, err
 }
 
 type UDPRelay struct {
@@ -55,49 +87,79 @@ type UDPRelay struct {
 	tunnels map[string]*UDPTunnel
 }
 
-func (r *UDPRelay) grabTunnel(localConn *net.UDPConn, cliaddr *net.UDPAddr) *UDPTunnel {
+func (r *UDPRelay) grabTunnel(localConn *net.UDPConn, clientAddr *net.UDPAddr) *UDPTunnel {
 	r.lock.Lock()
 	defer r.lock.Unlock()
-	addr := cliaddr.String()
+	addr := clientAddr.String()
 	tunnel := r.tunnels[addr]
 	if tunnel == nil {
-		port := uint16(cliaddr.Port)
+		port := uint16(clientAddr.Port)
 		session := r.nat.getSession(port)
 		if session == nil {
+			logger.Errorf("[udp] %s:%d > %s:%d no session", session.srcIP, session.srcPort, session.dstIP, session.dstPort)
 			return nil
-		}
-		record := r.one.dnsTable.GetByIP(session.dstIP)
-		if record == nil || record.RealIP == nil {
-			return nil
-		}
-		srvaddr := &net.UDPAddr{IP: record.RealIP, Port: int(session.dstPort)}
-		remoteConn, err := net.DialUDP("udp", nil, srvaddr)
-		if err != nil {
-			logger.Errorf("[udp] connect to %s failed: %v", srvaddr, err)
-			return nil
-		}
-		tunnel = &UDPTunnel{
-			session:    session,
-			record:     record,
-			cliaddr:    cliaddr,
-			localConn:  localConn,
-			remoteConn: remoteConn,
 		}
 
-		logger.Debugf("[udp] %s:%d > %v: new tunnel", session.srcIP, session.srcPort, srvaddr)
+		one := r.one
+		var host, proxy string
+		if record := one.dnsTable.GetByIP(session.dstIP); record != nil {
+			host = record.Hostname
+			proxy = record.Proxy
+		} else if one.dnsTable.Contains(session.dstIP) {
+			logger.Debugf("[udp] %s:%d > %s:%d dns expired", session.srcIP, session.srcPort, session.dstIP, session.dstPort)
+			return nil
+		} else {
+			host = session.dstIP.String()
+		}
+		remoteAddr := fmt.Sprintf("%s:%d", host, session.dstPort)
+		logger.Debugf("[udp] %s:%d > %s proxy %q", session.srcIP, session.srcPort, remoteAddr, proxy)
+		if remoteAddr == "" {
+			return nil
+		}
+
+		proxies := one.proxies
+		socks5TCPConn, err := proxies.Dial("udp", proxy, remoteAddr)
+		if err != nil {
+			logger.Errorf("[udp] dial %s by proxy %q failed: %s", remoteAddr, proxy, err)
+			return nil
+		}
+
+		socks5UDPListen, socks5Reply, err := socks5Proxy.Socks5UDPRequest(socks5TCPConn, "0.0.0.0", 0)
+
+		var hostType byte
+		if ip := net.ParseIP(host); ip != nil {
+			if ip4 := ip.To4(); ip4 != nil {
+				hostType = socks5Proxy.Socks5AtypIP4
+			} else {
+				hostType = socks5Proxy.Socks5AtypIP6
+			}
+		} else {
+			hostType = socks5Proxy.Socks5AtypDomain
+		}
+
+		tunnel = &UDPTunnel{
+			session:        session,
+			clientAddr:     clientAddr,
+			localConn:      localConn,
+			remoteUDPConn:  socks5UDPListen,
+			remoteTCPConn:  socks5TCPConn,
+			remoteHostType: hostType,
+			remoteHost:     host,
+			remotePort:     session.dstPort,
+			BndHost:        socks5Reply.BndHost,
+			BndPort:        socks5Reply.BndPort,
+		}
+
+		logger.Debugf("[udp] %s:%d > %v: new tunnel", session.srcIP, session.srcPort, remoteAddr)
 
 		r.tunnels[addr] = tunnel
 		go func() {
 			err := tunnel.Pump()
 			if err != nil {
-				logger.Debugf("[udp] pump to %v failed: %v", tunnel.remoteConn.RemoteAddr(), err)
+				logger.Errorf("[udp] pump to %v failed: %v", tunnel.remoteUDPConn.RemoteAddr(), err)
 			}
-			tunnel.remoteConn.Close()
-			logger.Debugf("[udp] %s:%d > %v: destroy tunnel", tunnel.session.srcIP, tunnel.session.srcPort, srvaddr)
-
-			r.lock.Lock()
-			delete(r.tunnels, addr)
-			r.lock.Unlock()
+			logger.Debugf("[udp] %s:%d > %v: destroy tunnel", tunnel.session.srcIP, tunnel.session.srcPort, remoteAddr)
+			r.close(tunnel, addr)
 		}()
 	}
 	tunnel.SetDeadline(NatSessionLifeSeconds * time.Second)
@@ -112,8 +174,17 @@ func (r *UDPRelay) handlePacket(localConn *net.UDPConn, cliaddr *net.UDPAddr, pa
 	}
 	_, err := tunnel.Write(packet)
 	if err != nil {
-		logger.Debugf("[udp] %v", err)
+		logger.Errorf("[udp] %v", err)
 	}
+}
+
+func (r *UDPRelay) close(tunnel *UDPTunnel, addr string) {
+	tunnel.remoteUDPConn.Close()
+	tunnel.remoteTCPConn.Close()
+
+	r.lock.Lock()
+	delete(r.tunnels, addr)
+	r.lock.Unlock()
 }
 
 func (r *UDPRelay) Serve() error {
@@ -125,11 +196,11 @@ func (r *UDPRelay) Serve() error {
 
 	for {
 		b := make([]byte, MTU)
-		n, cliaddr, err := conn.ReadFromUDP(b)
+		n, clientAddr, err := conn.ReadFromUDP(b)
 		if err != nil {
 			return err
 		}
-		go r.handlePacket(conn, cliaddr, b[:n])
+		go r.handlePacket(conn, clientAddr, b[:n])
 	}
 }
 
@@ -169,8 +240,18 @@ func (r *UDPRelay) Filter(wr io.Writer, ipPacket tcpip.IPv4Packet) {
 				srcIP, srcPort, dstIP, dstPort, dstIP, port, r.relayIP, r.relayPort)
 		}
 	} else {
-		logger.Errorf("[udp] %s:%d > %s:%d: invalid packet", srcIP, srcPort, dstIP, dstPort)
-		return
+		// redirect to relay
+		isNew, port := r.nat.allocSession(srcIP, dstIP, srcPort, dstPort)
+
+		ipPacket.SetSourceIP(dstIP)
+		udpPacket.SetSourcePort(port)
+		ipPacket.SetDestinationIP(r.relayIP)
+		udpPacket.SetDestinationPort(r.relayPort)
+
+		if isNew {
+			logger.Debugf("[udp] %s:%d > %s:%d: shape to %s:%d > %s:%d",
+				srcIP, srcPort, dstIP, dstPort, dstIP, port, r.relayIP, r.relayPort)
+		}
 	}
 
 	// write back packet
